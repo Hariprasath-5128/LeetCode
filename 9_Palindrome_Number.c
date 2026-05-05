2600fd70570b22892ce977e3404d71a6b418f4fa
Hariprasath-5128 <hariprasath1528@gmail.com>
2026-05-06 01:23:28 +0530

    3

--- 9_Palindrome_Number.c
+++ 9_Palindrome_Number.c
@@ -1,23 +1,22 @@
-class Solution {
-    public boolean isPalindrome(int x) {
-        bool isPalindrome(int x) {
+#include <stdbool.h>
+#include <limits.h>
+
+bool isPalindrome(int x) {
     if (x < 0) return false;
 
-        int original = x;
-        int rev = 0;
+    int original = x;
+    int rev = 0;
 
-        while (x > 0) {
-            int digit = x % 10;
+    while (x > 0) {
+        int digit = x % 10;
 
-            if(rev>(INT_MAX-digit)/10)  /*Checking the overflow condition*/
-            {
-               return false;
-            }
-            rev = rev * 10 + digit;
-            x /= 10;
+        if (rev > (INT_MAX - digit) / 10) {
+            return false;
         }
 
-        return original == rev;
-}
+        rev = rev * 10 + digit;
+        x /= 10;
     }
+
+    return original == rev;
 }
