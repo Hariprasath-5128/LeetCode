9b19edcec1999f37eda5e9f4478192b88ac61b34
Hariprasath-5128 <hariprasath1528@gmail.com>
2026-08-13 20:35:31 +0530

    73

---
+++ 73_Set_Matrix_Zeroes.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> row = new HashSet();
+        Set<Integer> col = new HashSet();
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if(matrix[i][j]==0){
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+
+        for(int x: row){
+            for(int i = 0; i < matrix[0].length; i++){
+                matrix[x][i] = 0;
+            }
+        }
+
+        for(int x: col){
+            for(int i = 0; i < matrix.length; i++){
+                matrix[i][x] = 0;
+            }
+        }
+    }
+}
