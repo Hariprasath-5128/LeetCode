83f3af58abb5202cde108f627644942ffe2184d8
Hariprasath-5128 <hariprasath1528@gmail.com>
2026-09-08 23:45:15 +0530

    Binary Search - Easy

---
+++ 278_First_Bad_Version.c
@@ -0,0 +1,23 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    
+    int bad = -1;
+    int start = 1;
+    int end = n;
+
+    while(start <= end){
+        int mid = start + (end - start)/2 ;
+        if(isBadVersion(mid)){
+            bad = mid;
+            end = mid - 1;
+        }
+
+        else{
+            start = mid + 1;
+        }
+    }
+
+    return bad;
+}
